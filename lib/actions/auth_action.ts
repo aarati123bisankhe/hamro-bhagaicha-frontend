12623c8c3ac641db959d8b7ad64b6c4bb32afc28
Aarati Thapa <aaratithapa@Aaratis-MacBook-Air.local>
2026-02-14 13:33:02 +0545

    update the actions and api

--- lib/actions/auth_action.ts
+++ lib/actions/auth_action.ts
@@ -79,7 +79,7 @@
 'use server'
 
 import { revalidatePath } from 'next/cache';
-import {login, register, updateProfile, whoAmI} from '../api/auth';
+import {login, register, requestPasswordReset, resetPassword, updateProfile, whoAmI} from '../api/auth';
 import { setAuthToken, setUserData } from '../cookie';
 import { LoginData, RegisterData } from '@/app/(auth)/schema';
 
@@ -157,3 +157,34 @@ export async function handleUpdateProfile(profileData: FormData) {
         return { success: false, message: error.message };
     }
 }
+
+
+export const handleRequestPasswordReset = async (email: string) => {
+    try {
+        const response = await requestPasswordReset(email);
+        if (response.success) {
+            return {
+                success: true,
+                message: 'Password reset email sent successfully'
+            }
+        }
+        return { success: false, message: response.message || 'Request password reset failed' }
+    } catch (error: Error | any) {
+        return { success: false, message: error.message || 'Request password reset action failed' }
+    }
+};
+
+export const handleResetPassword = async (token: string, newPassword: string) => {
+    try {
+        const response = await resetPassword(token, newPassword);
+        if (response.success) {
+            return {
+                success: true,
+                message: 'Password has been reset successfully'
+            }
+        }
+        return { success: false, message: response.message || 'Reset password failed' }
+    } catch (error: Error | any) {
+        return { success: false, message: error.message || 'Reset password action failed' }
+    }
+};
--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -4,12 +4,15 @@ export const API = {
         REGISTER: "/api/auth/register",
         WHOAMI: '/api/auth/whoami',
         UPDATEPROFILE: '/api/auth/update-profile',
+        REQUEST_PASSWORD_RESET: '/api/auth/send-reset-password-email',
+        RESET_PASSWORD: (token: string) => `/api/auth/reset-password/${token}`,
     },
 
       ADMIN:{
         USER:{
             CREATE: '/api/admin/users',
-            ALL: "/api/admin/users", 
+            ALL: "/api/admin/users",
+            DELETE: (userId: string) => `/api/admin/users/${userId}` 
         }
     }
 }
