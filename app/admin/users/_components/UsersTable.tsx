214e7e57a6554896599d4336e745398de816492c
Aarati Thapa <aaratithapa@Aaratis-MacBook-Air.local>
2026-02-25 14:29:49 +0545

    admin done

--- app/admin/users/_components/UsersTable.tsx
+++ app/admin/users/_components/UsersTable.tsx
@@ -1,34 +1,3 @@
-// "use client";
-
-// import { AdminUser } from "../page";
-
-// export default function UsersTable({ users }: { users: AdminUser[] }) {
-//   return (
-//     <div className="bg-white rounded-md shadow overflow-x-auto">
-//       <table className="w-full min-w-[600px]">
-//         <thead className="bg-gray-100">
-//           <tr>
-//             <th className="p-3 text-left">Name</th>
-//             <th className="p-3 text-left">Email</th>
-//             <th className="p-3 text-left">Phone</th>
-//             <th className="p-3 text-left">Role</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {users.map((u) => (
-//             <tr key={u._id} className="border-t hover:bg-gray-50">
-//               <td className="p-3">{u.fullName}</td>
-//               <td className="p-3">{u.email}</td>
-//               <td className="p-3">{u.phoneNumber}</td>
-//               <td className="p-3 capitalize">{u.role}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
 "use client";
 
 import Image from "next/image";
@@ -51,7 +20,6 @@ export default function UsersTable({
   return (
     <div className="bg-white rounded-md shadow overflow-x-auto">
       <table className="w-full min-w-[700px] text-sm">
-        {/* ================= HEADER ================= */}
         <thead className="bg-gray-100 text-left">
           <tr>
             <th className="p-3">User</th>
