01829a2cbecfb40ce8559d7c1308bf69f8217b49
Aarati Thapa <aaratithapa@Aaratis-MacBook-Air.local>
2026-02-25 14:27:30 +0545

    check the files

--- app/admin/profile/schema.tsx
+++ app/admin/profile/schema.tsx
@@ -1,5 +1,5 @@
 import z from "zod";
-const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 
 export const updateAdminSchema = z.object({
